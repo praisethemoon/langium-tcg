23d080ea106b0b68ff238d0036407913fc9255c2
praisethemoon <doit@praisethemoon.org>
2025-01-10 20:34:01 +0100

    fixed scoping for good (i think)

--- src/language/card-dsl-scope.ts
+++ src/language/card-dsl-scope.ts
@@ -1,5 +1,5 @@
 import { AstNode, DefaultScopeProvider, LangiumCoreServices, ReferenceInfo, Scope } from "langium";
-import { Ability,  isAbility, isSelectStep, SelectStep, VariableDecl, isTrapCard, TrapCard, isSpellCard, SpellCard } from "./generated/ast.js";
+import { isAbility, isSelectStep, VariableDecl, isTrapCard, isSpellCard } from "./generated/ast.js";
 
 export class CardDslScopeProvider extends DefaultScopeProvider {
     constructor(services: LangiumCoreServices) {
@@ -15,127 +15,65 @@ export class CardDslScopeProvider extends DefaultScopeProvider {
         }
 
         // Special case: handle variables used in their own where clause
-        const selectStep = this.findContainingSelectStep(context.container);
-        if (selectStep) {
-            const declarations = this.getScopeForSelectStep(context.container, selectStep);
-            if (declarations.length > 0) {
-                return this.createScopeForNodes(declarations);
-            }
-        }
-
-        // Get the containing ability
-        const ability = this.findContainingAbility(context.container);
-        if(ability) {
-            // Get all variables declared in this ability
-            const declarations = this.getAllVariableDeclarationsInAbility(ability);
-            return this.createScopeForNodes(declarations);
-        }
-
-        // check if we have a trigger block
-        const trapCard = this.findContainingTrapCard(context.container);
-        if(trapCard) {
-            // Get all variables declared in this ability
-            const declarations = this.getAllVariableDeclarationsInTrapCard(trapCard);
-            return this.createScopeForNodes(declarations);
-        }
-
-        const spellCard = this.findContainingSpellCard(context.container);
-        if(spellCard) {
-            // Get all variables declared in this ability
-            const declarations = this.getAllVariableDeclarationsInSpellCard(spellCard);
-            return this.createScopeForNodes(declarations);
+        const vars = this.findReachableVariables(context.container);
+        if (vars.length > 0) {
+            return this.createScopeForNodes(vars);
         }
         return super.getScope(context);
     }
 
 
-    private findContainingTrapCard(node: AstNode | undefined): TrapCard | undefined {
-        while (node) {
-            if (isTrapCard(node)) {
-                return node;
-            }
-            node = node.$container;
-        }
-        return undefined;
-    }
-
-    private findContainingSelectStep(node: AstNode | undefined): SelectStep | undefined {
-        while (node) {
+    /**
+     * Finds all variables that are reachable from the given node.
+     * It starts upwards in its own container, until we reach a select step.
+     *   1. Add select step's variable
+     *   2. Iterate over container's children, until we reach this select step
+     * 
+     */
+    private findReachableVariables(node: AstNode | undefined): VariableDecl[] {
+        let variables: VariableDecl[] = [];
+        while ((node?.$container != undefined)) {
             if (isSelectStep(node)) {
-                return node;
+                variables.push(node.variable);
             }
-            node = node.$container;
-        }
-        return undefined;
-    }
 
-    private getScopeForSelectStep(currentNode: AstNode, selectStep: SelectStep): VariableDecl[] {
-        // If we're in a where clause and the variable reference is using array indexing
-        // we should allow access to the variable being declared
-        if (selectStep.condition && this.isNodeContainedIn(currentNode, selectStep.condition)) {
-            return [selectStep.variable];
-        }
-        return [];
-    }
+            const currentStepMarker = node;
 
-    private findContainingAbility(node: AstNode | undefined): Ability | undefined {
-        while (node) {
-            if (isAbility(node)) {
-                return node;
+            const parent = node.$container;
+
+            // spell card uses AbilityBlock fragment, so it is kind of an ability
+            if(isTrapCard(parent)) {
+                if (parent.trigger.event.target) {
+                    variables.push(parent.trigger.event.target);
+                }
+                if (parent.trigger.event.attacked) {
+                    variables.push(parent.trigger.event.attacked);
+                }
             }
-            node = node.$container;
-        }
-        return undefined;
-    }
 
-    private getAllVariableDeclarationsInAbility(ability: Ability): VariableDecl[] {
-        const declarations: VariableDecl[] = [];
-        const steps = ability.steps ?? [];
+            if(isAbility(parent) || isSpellCard(parent) || isTrapCard(parent)) {
+                if(isAbility(parent)) {
+                    if(parent.trigger?.event?.target) {
+                        variables.push(parent.trigger.event.target);
+                    }
+                    if(parent.trigger?.event?.attacked) {
+                        variables.push(parent.trigger.event.attacked);
+                    }
+                }
 
-        for (const step of steps) {
-            if (isSelectStep(step)) {
-                declarations.push(step.variable);
+                for(const step of parent.steps) {
+                    if(isSelectStep(step)) {
+                        variables.push(step.variable);
+                        if(step === currentStepMarker) {
+                            break;
+                        }
+                    }
+                }
             }
-        }
 
-        return declarations;
+            node = parent;
+        }
+        return variables;
     }
 
-    private getAllVariableDeclarationsInTrapCard(trapCard: TrapCard): VariableDecl[] {
-        const declarations: VariableDecl[] = [];
-        if(trapCard.trigger.event.target) {
-            declarations.push(trapCard.trigger.event.target);
-        }
-        if(trapCard.trigger.event.attacked) {
-            declarations.push(trapCard.trigger.event.attacked);
-        }
-        return declarations;
-    }
-
-    private findContainingSpellCard(node: AstNode | undefined): SpellCard | undefined {
-        while (node) {
-            if (isSpellCard(node)) {
-                return node;
-            }
-            node = node.$container;
-        }
-        return undefined;
-    }
-
-    private getAllVariableDeclarationsInSpellCard(spellCard: SpellCard): VariableDecl[] {
-        const declarations: VariableDecl[] = [];
-        for (const step of spellCard.steps) {
-            if (isSelectStep(step)) {
-                declarations.push(step.variable);
-            }
-        }
-        return declarations;
-    }
-
-    private isNodeContainedIn(node: AstNode, container: AstNode): boolean {
-        while (node && node !== container) {
-            node = node.$container!;
-        }
-        return node === container;
-    }
 }
